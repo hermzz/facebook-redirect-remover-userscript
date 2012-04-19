5852800c36294c090b1313c6c1924b7ff9ab3142
Hermann Kaser <hermann.kaser@gmail.com>
2012-04-19 11:50:06 +0200

    Replace links in hovercard as well

--- facebook.user.js
+++ facebook.user.js
@@ -5,18 +5,37 @@
 // @match 		https://*.facebook.com/*
 // ==/UserScript
 
-console.log('Replacing URIs');
-containers = document.getElementsByClassName('external uiAttachmentMedia')
-for(var i=0; i<containers.length; i++)
+function replace_uris()
 {
-	c = containers[i];
-	r = c.href.match(/redirect_uri=(.+)%3F/);
-	if(r)
+	// replace normal news items
+	containers = document.getElementsByClassName('external uiAttachmentMedia');
+	for(var i=0; i<containers.length; i++)
 	{
-		url=decodeURIComponent(r[1]);
-		console.log('replacing url ' + url);
-		c.href = url;
-		c.nextSibling.childNodes[0].childNodes[0].childNodes[0].href = url;
-		c.parentElement.parentElement.previousSibling.childNodes[2].href= url;
+		c = containers[i];
+		r = c.href.match(/redirect_uri=(.+)%3F/);
+		if(r)
+		{
+			url = decodeURIComponent(r[1]);
+			c.href = url;
+			c.nextSibling.childNodes[0].childNodes[0].childNodes[0].href = url;
+			c.parentElement.parentElement.previousSibling.childNodes[2].href= url;
+		}
+	}
+
+	// replace links on popup hovercard thingies
+	containers = document.getElementsByClassName('HovercardStage');
+	for(var i=0; i<containers.length; i++)
+	{
+		c = containers[i];
+		href = c.childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[0].href;
+		r = href.match(/redirect_uri=(.+)%3F/);
+
+		if(r)
+			c.childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[0].href = decodeURIComponent(r[1]);
 	}
 }
+
+// listen to changes in the DOM
+document.addEventListener('DOMSubtreeModified', replace_uris, false);
+
+replace_uris();
